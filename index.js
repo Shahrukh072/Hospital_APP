d7dd439d6f14d12a412fa3b628dd8e4cb64939f9
shahrukh khan <37824383+Shahrukh072@users.noreply.github.com>
2022-08-27 23:14:40 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 //require
 const express = require('express');
-const port=8000;
 const bodyParser = require('body-parser');
-const db = require('./config/mongoose');
 const app = express();
+const port=8000;
+const db = require('./config/mongoose');
+const patients = require('./routes/patients');
+const doctors = require('./routes/doctors');
+const jwt = require('jsonwebtoken');
 const passport = require("passport");
 const passportJwt = require("./config/passport-jwt-startegy");
 
 app.use(bodyParser.urlencoded());
+
+app.set('secretKey', 'api');
+app.use(bodyParser.urlencoded({
+    extended: false 
+}));
+app.use('/doctors', doctors);
+app.use('/patients', validateUser, patients);
+
+
+//validate jwt
+function validateUser(req, res, next) {
+    jwt.verify(req.headers['x-access-token'], req.app.get('secretKey'), function (err, decoded) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err.message,
+                data: null
+            });
+        } else {
+            req.body.userId = decoded.id;
+            next();
+        }
+    });
+}
+
+app.get('/', function (req, res) {
+    res.json({
+        "body": "hospital app"
+    });
+});
+
 
 //routes
 app.use("/", require("./routes"));
@@ -18,4 +52,4 @@ app.listen(port, function () {
 });
 
 
-module.exports =app;
+// module.exports =app;
